a7e6a5a36bd6759fb86f86955f4b0c6663b0ccf8
davini10c <vinicio_520@hotmail.com>
2019-02-12 14:18:35 -0500

    login

--- Supletorio/app/src/main/java/com/example/viniq/supletorio/MainActivity.kt
+++ Supletorio/app/src/main/java/com/example/viniq/supletorio/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.viniq.supletorio
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button3_registrarse.setOnClickListener {
+            this.irRegistrar()
+        }
+
+
+        button4_login.setOnClickListener {
+            this.irMenuPrincipal()
+        }
+    }
+
+
+    fun irRegistrar() {
+        val registar = Intent(this, RegistrarseActivity::class.java)
+        startActivity(registar)
+    }
+
+    fun irMenuPrincipal() {
+        val menuPrincipal = Intent(this, MenuPrincipalActivity::class.java)
+        startActivity(menuPrincipal)
     }
 }
---
+++ backend/api/controllers/UsuarioController.js
@@ -0,0 +1,29 @@
+/**
+ * UsuarioController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+login: async (req, res) =>{
+const parametros = req.allParams();
+var usuarioLogeado = await
+Usuario.find({
+where:{
+correo:parametros.correo,
+password: parametros.password,
+
+}
+});
+
+if(usuarioLogeado){
+return res.ok(usuarioLogeado);
+}
+else {
+return res.badRequest({mensaje: 'Usuario Invalido'});
+}
+},
+};
+
